5d2c2a9c91fd9c35d0abd1a6d15ffde04eb6c124
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-07-29 15:00:53 -0300

    modifico selector para post_author (usando soup)

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -169,6 +169,16 @@ def bs4_parse(html):
     like_count = post_json.get("edge_media_preview_like").get("count")
     if like_count: post_like_count = int(like_count)
     post_shortcode = post_json.get("shortcode")
+    # like_count = post_json.get("edge_media_preview_like").get("count")
+    # if like_count:
+    #     post_like_count = int(like_count)
+
+    try:
+        username = soup.select('div._4g34._5i2i._52we h3 a')[0]
+        post_author = username.string
+    except (IndexError, TypeError):
+        print("Error: post_author")
+
     try:
         owner = soup.select('#u_0_z')[0]
         owner = json.loads(owner['data-store'])
